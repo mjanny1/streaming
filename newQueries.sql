28dbc0cc2e67576740d12cd3fcac9a5962b9cf7a
Matthew Janny <mattjanny1@gmail.com>
2021-05-05 21:54:00 -0400

    Adding Ravi's alg dev queries

--- newQueries.sql
+++ newQueries.sql
@@ -77,3 +77,86 @@ on ep.tv_show_id = ts.tv_show_id
 join media m
 on m.media_id = ep.media_id
 where ts.name = 'Game of Thrones';
+
+
+--As an Algorithm Developer, I want to know the hours spent on a piece of media
+
+select m.media_id,v.time,v.location, v.seconds_watched/360 as hours_spent, m.name from 
+views v join media m on v.media_id = m.media_id 
+join profile p on v.profile_id  = p.profile_id 
+join media_preferences mp on v.profile_id = mp.profile_id;
+
+--Fetching the user names of people who liked the media
+
+select m.name, mp.media_id, a.username,mp.liked from media_preferences mp 
+join media m on mp.media_id = m.media_id 
+join profile p on mp.profile_id = p.profile_id 
+join accounts a on p.account_id = a.account_id  where liked = 'Y';
+
+
+--Fetching media what particular user is watching 
+select m.name, mp.media_id, a.username,mp.liked from media_preferences mp 
+join media m on mp.media_id = m.media_id 
+join profile p on mp.profile_id = p.profile_id 
+join accounts a on p.account_id = a.account_id where a.username = 'watcher12'
+
+--As an algorithm developer, I want to capture what genre the user is watching so that I can better determine what the user likes to watch.
+
+select m.name, a.username,mp.liked,mp.media_id,g.comedy,g.action,g.romance,g.horror,g.drama from media_preferences mp 
+join genres g on mp.media_id = g.media_id
+join media m on mp.media_id = m.media_id 
+join profile p on mp.profile_id = p.profile_id 
+join accounts a on p.account_id = a.account_id where mp.liked = 'Y' and a.username = 'watcher12';
+
+--As an algorithm developer, I want to know the length of the episodes and movies a person is watching so that I can make better media recommendations.
+
+select m.name, a.username,m.length_seconds/60 as Length_minutes,mp.media_id from media_preferences mp 
+join genres g on mp.media_id = g.media_id
+join media m on mp.media_id = m.media_id 
+join profile p on mp.profile_id = p.profile_id 
+join accounts a on p.account_id = a.account_id where  a.username = 'watcher12';
+
+--Count of movies watched by a user of a particular director
+
+Select Count(M.media_id) from media m 
+join directs d on m.media_id = d.media_id 
+join views v on v.media_id = m.media_id
+join industry_person ip on d.industry_person_id = ip.industry_person_id 
+where v.profile_id = 2 and ip.first_name = 'Anthony' and ip.last_name = 'Russo';
+
+--count of movies watched by a user of a particular actor
+Select Count(M.media_id) from media m 
+join actsin ac on m.media_id = ac.media_id 
+join views v on v.media_id = m.media_id
+join industry_person ip on ac.industry_person_id = ip.industry_person_id 
+where v.profile_id = 1 and ip.first_name = 'Jennifer' and ip.last_name = 'Garner';
+
+--As an algorithm developer, I want to know if a user liked the episode, or movie so that the algorithm can make better media recommendations.
+select m.name, a.username,mp.liked from media_preferences mp 
+join media m on mp.media_id = m.media_id 
+join profile p on mp.profile_id = p.profile_id 
+join accounts a on p.account_id = a.account_id where a.username = 'watcher12' and m.name = 'John Wick';
+
+--As an algorithm developer, I want to know when a user watched a piece of media so that the algorithm can make better recommendations based off time of year (i.e. seasons, holidays)
+select m.name, a.username,v.time, mp.liked from media_preferences mp 
+join media m on mp.media_id = m.media_id 
+join views v on m.media_id = v.media_id
+join profile p on mp.profile_id = p.profile_id 
+join accounts a on p.account_id = a.account_id where a.username = 'watcher12';
+
+--As an algorithm developer, I want to know the Location of where a profile is watching a movie or tv show so that I can look for trends and make better recommendations based on what part of the world someone is from.
+
+select m.name, a.username,v.location, mp.liked from media_preferences mp 
+join media m on mp.media_id = m.media_id 
+join views v on m.media_id = v.media_id
+join profile p on mp.profile_id = p.profile_id 
+join accounts a on p.account_id = a.account_id where a.username = 'watcher12';
+
+--As an algorithm developer, I want to know the age, gender of the person watching so that I can make better media recommendations.
+
+select a.username,p.name,p.age, p.gender from media_preferences mp 
+join media m on mp.media_id = m.media_id 
+join views v on m.media_id = v.media_id
+join profile p on mp.profile_id = p.profile_id 
+join accounts a on p.account_id = a.account_id where m.name = 'The Old Gods and the New';
+
