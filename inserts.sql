5d3bbba693ba04357bc3291f0ee4e9416d02710a
Marshall Dugan <marshall.dugan@gmail.com>
2021-05-05 22:08:57 -0400

    Added new insert statements to match spreedsheet

--- inserts.sql
+++ inserts.sql
@@ -1,10 +1,10 @@
 --Media
-INSERT INTO Media VALUES('M0001', 'Movie', 'Fast And Furious', to_date('01-02-1996', 'dd-mm-yyyy'), 1,199772, 'English', 'good movie');
-INSERT INTO Media VALUES('M0002', 'Movie', 'John Wick', to_date('01-02-1997', 'dd-mm-yyyy'), 2,736363, 'Spanish', 'great one');
-INSERT INTO Media VALUES('M0003', 'Movie', 'Finding Nemo', to_date('01-02-1998', 'dd-mm-yyyy'), 3,2378328, 'German', 'a fish movie');
-INSERT INTO Media VALUES('M0005', 'Movie', 'Mission Impossible', to_date('01-02-1999', 'dd-mm-yyyy'), 4,88737, 'English', 'great action');
-INSERT INTO Media VALUES('T0001', 'Episode', 'GOT2.2', to_date('01-02-1992', 'dd-mm-yyyy'), 5.5,44535, 'Spanish', 'Random People die');
-INSERT INTO Media VALUES('T0002', 'Episode', 'BreakingBad', to_date('01-02-1995', 'dd-mm-yyyy'), 9,53737, 'German', 'Makes blue drugs');
+INSERT INTO Media VALUES('M0001', 'Movie', 'Fast And Furious', to_date('01-02-1996', 'mm-dd-yyyy'), 1,199772, 'English', 'good movie');
+INSERT INTO Media VALUES('M0002', 'Movie', 'John Wick', to_date('01-02-1997', 'mm-dd-yyyy'), 2,736363, 'Spanish', 'great one');
+INSERT INTO Media VALUES('M0003', 'Movie', 'Finding Nemo', to_date('01-02-1998', 'mm-dd-yyyy'), 3,2378328, 'German', 'a fish movie');
+INSERT INTO Media VALUES('M0005', 'Movie', 'Mission Impossible', to_date('01-02-1999', 'mm-dd-yyyy'), 4,88737, 'English', 'great action');
+INSERT INTO Media VALUES('T0001', 'Episode', 'GOT2.2', to_date('01-02-1992', 'mm-dd-yyyy'), 5.5,44535, 'Spanish', 'Random People die');
+INSERT INTO Media VALUES('T0002', 'Episode', 'BreakingBad', to_date('01-02-1995', 'mm-dd-yyyy'), 9,53737, 'German', 'Makes blue drugs');
 INSERT INTO Media VALUES('T0003', 'Episode', 'Winter is Coming', to_date('04-17-2011', 'mm-dd-yyyy'), 9,3601, 'English', 'Ned Stark is torn');
 INSERT INTO Media VALUES('T0004', 'Episode', 'The Kingsroad', to_date('04-24-2011', 'mm-dd-yyyy'), 8.9,3605, 'English', 'Bran recovers');
 INSERT INTO Media VALUES('T0005', 'Episode', 'Lord Snow', to_date('05-01-2011', 'mm-dd-yyyy'), 8.8,3568, 'English', 'Jon begins training');
@@ -25,12 +25,46 @@ INSERT INTO Media VALUES('T0019', 'Episode', 'A Man Without Honor', to_date('05-
 INSERT INTO Media VALUES('T0020', 'Episode', 'The Prince of Winterfell', to_date('05-20-2012', 'mm-dd-yyyy'), 9.1,3562, 'English', 'Stannis arrives to Kings Landing');
 INSERT INTO Media VALUES('T0021', 'Episode', 'BlackWater', to_date('05-27-2012', 'mm-dd-yyyy'), 9.2,3612, 'English', 'Stannis brings his army');
 INSERT INTO Media VALUES('T0022', 'Episode', 'Valar Moghulis', to_date('06-03-2012', 'mm-dd-yyyy'), 9.7,3614, 'English', 'Arya says farewall to Jaquen');
+INSERT INTO Media VALUES('M0004', 'Movie', 'Iron Man', to_date('01-04-2008', 'mm-dd-yyyy'), 8.9,6198, 'English', 'Tony Stark a billionaire becomes iron man');
+INSERT INTO Media VALUES('M0006', 'Movie', 'Iron Man 2', to_date('09-23-2010', 'mm-dd-yyyy'), 9.6,6100, 'English', 'Tony Stark saves the world');
+INSERT INTO Media VALUES('M0007', 'Movie', 'Iron Man 3', to_date('08-21-2013', 'mm-dd-yyyy'), 7.6,6209, 'English', 'Tony Stark Saves the world again');
+INSERT INTO Media VALUES('M0008', 'Movie', 'The Incredible Hulk', to_date('08-13-2008', 'mm-dd-yyyy'), 8.2,6710, 'English', 'Bruce Banner turns into Hulk');
+INSERT INTO Media VALUES('M0009', 'Movie', 'Thor', to_date('04-07-2011', 'mm-dd-yyyy'), 8.8,7000, 'English', 'Origins and first visit to Earth');
+INSERT INTO Media VALUES('M0010', 'Movie', 'Thor: The Dark World', to_date('05-09-2013', 'mm-dd-yyyy'), 7.5,6230, 'English', 'Battle of Sokovia');
+INSERT INTO Media VALUES('M0011', 'Movie', 'Thor: Ragnarok', to_date('12-09-2017', 'mm-dd-yyyy'), 7.2,6703, 'English', 'Battle with the Dark Elves');
+INSERT INTO Media VALUES('M0012', 'Movie', 'Thor: Love and Thunder', to_date('07-18-2020', 'mm-dd-yyyy'), 8.1,6902, 'English', 'Reversing the Blip');
+INSERT INTO Media VALUES('M0013', 'Movie', 'Captain America: The First Avenger', to_date('09-21-2011', 'mm-dd-yyyy'), 7.6,6095, 'English', 'Steve Rogers, a frail man, is transformed into the super-soldier Captain America');
+INSERT INTO Media VALUES('M0015', 'Movie', 'Captain America: The Winter Soldier', to_date('06-09-2014', 'mm-dd-yyyy'), 8.4,6720, 'English', 'Captain finds his long lost friend');
+INSERT INTO Media VALUES('M0016', 'Movie', 'Captain America: Civil War', to_date('08-06-2016', 'mm-dd-yyyy'), 8.9,6103, 'English', 'Tony stark and Steve rogers fight each other  ');
+INSERT INTO Media VALUES('M0017', 'Movie', 'Avengers: Infinity War', to_date('07-08-2018', 'mm-dd-yyyy'), 9,6730, 'English', 'A group of super heros save the world ');
+INSERT INTO Media VALUES('M0018', 'Movie', 'Avengers: Endgame', to_date('09-05-2019', 'mm-dd-yyyy'), 8.6,6490, 'English', 'A group of super heros save the world again');
+INSERT INTO Media VALUES('M0019', 'Movie', 'Guardians of the Galaxy', to_date('03-16-2014', 'mm-dd-yyyy'), 7.8,6420, 'English', 'Brash space adventurer Peter Quill (Chris Pratt) finds himself the quarry of relentless bounty hunters after he steals an orb coveted by Ronan, a powerful villain.');
+INSERT INTO Media VALUES('M0020', 'Movie', 'Guardians of the Galaxy Vol. 2', to_date('08-02-2017', 'mm-dd-yyyy'), 6.9,6890, 'English', 'Peter Quill and his fellow Guardians are hired by a powerful alien race, the Sovereign, to protect their precious batteries from invaders.');
+INSERT INTO Media VALUES('M0021', 'Movie', 'Ant-Man and the Wasp', to_date('09-15-2018', 'mm-dd-yyyy'), 7.9,6201, 'English', 'Scott Lang is grappling with the consequences of his choices as both a superhero and a father.');
+INSERT INTO Media VALUES('M0022', 'Movie', 'Ant-Man', to_date('08-18-2015', 'mm-dd-yyyy'), 8.1,6201, 'English', 'Forced out of his own company by former protégé Darren Cross, Dr. Hank Pym recruits the talents of Scott Lang, a master thief just released from prison.');
 
 --Movies
 INSERT INTO Movies VALUES('M0001', 'G');
 INSERT INTO Movies VALUES('M0002', 'PG');
 INSERT INTO Movies VALUES('M0003', 'PG-13');
 INSERT INTO Movies VALUES('M0005', 'R');
+INSERT INTO Movies VALUES('M0004', 'NR');
+INSERT INTO Movies VALUES('M0006', 'PG-13');
+INSERT INTO Movies VALUES('M0007', 'PG-13');
+INSERT INTO Movies VALUES('M0008', 'PG-13');
+INSERT INTO Movies VALUES('M0009', 'PG-13');
+INSERT INTO Movies VALUES('M0010', 'PG-13');
+INSERT INTO Movies VALUES('M0011', 'PG-13');
+INSERT INTO Movies VALUES('M0012', 'PG');
+INSERT INTO Movies VALUES('M0013', 'PG');
+INSERT INTO Movies VALUES('M0015', 'PG');
+INSERT INTO Movies VALUES('M0016', 'PG');
+INSERT INTO Movies VALUES('M0017', 'PG');
+INSERT INTO Movies VALUES('M0018', 'G');
+INSERT INTO Movies VALUES('M0019', 'G');
+INSERT INTO Movies VALUES('M0020', 'G');
+INSERT INTO Movies VALUES('M0021', 'G');
+INSERT INTO Movies VALUES('M0022', 'G');
 
 --TV Shows
 INSERT INTO TV_Shows VALUES(0, 'Game of Thrones',9.6, '2');
@@ -72,6 +106,43 @@ INSERT INTO Genres VALUES('M0003', 'N', 'N', 'N', 'Y', 'Y');
 INSERT INTO Genres VALUES('M0005', 'N', 'Y', 'N', 'N', 'Y');
 INSERT INTO Genres VALUES('T0001', 'N', 'N', 'N', 'N', 'Y');
 INSERT INTO Genres VALUES('T0002', 'Y', 'Y', 'N', 'N', 'N');
+INSERT INTO Genres VALUES('T0003', 'N', 'Y', 'N', 'Y', 'Y');
+INSERT INTO Genres VALUES('T0004', 'N', 'Y', 'N', 'Y', 'Y');
+INSERT INTO Genres VALUES('T0005', 'N', 'Y', 'N', 'Y', 'Y');
+INSERT INTO Genres VALUES('T0006', 'N', 'Y', 'N', 'Y', 'Y');
+INSERT INTO Genres VALUES('T0007', 'N', 'Y', 'N', 'Y', 'Y');
+INSERT INTO Genres VALUES('T0008', 'N', 'Y', 'N', 'Y', 'Y');
+INSERT INTO Genres VALUES('T0009', 'N', 'Y', 'N', 'Y', 'Y');
+INSERT INTO Genres VALUES('T0010', 'N', 'Y', 'Y', 'Y', 'Y');
+INSERT INTO Genres VALUES('T0011', 'N', 'Y', 'Y', 'Y', 'Y');
+INSERT INTO Genres VALUES('T0012', 'N', 'Y', 'Y', 'Y', 'Y');
+INSERT INTO Genres VALUES('T0013', 'N', 'N', 'Y', 'N', 'Y');
+INSERT INTO Genres VALUES('T0014', 'N', 'N', 'Y', 'N', 'Y');
+INSERT INTO Genres VALUES('T0015', 'N', 'N', 'Y', 'N', 'Y');
+INSERT INTO Genres VALUES('T0016', 'N', 'N', 'Y', 'N', 'Y');
+INSERT INTO Genres VALUES('T0017', 'N', 'N', 'Y', 'N', 'Y');
+INSERT INTO Genres VALUES('T0018', 'N', 'N', 'Y', 'N', 'Y');
+INSERT INTO Genres VALUES('T0019', 'N', 'N', 'Y', 'N', 'Y');
+INSERT INTO Genres VALUES('T0020', 'N', 'N', 'Y', 'N', 'Y');
+INSERT INTO Genres VALUES('T0021', 'N', 'N', 'Y', 'N', 'Y');
+INSERT INTO Genres VALUES('T0022', 'N', 'N', 'N', 'N', 'Y');
+INSERT INTO Genres VALUES('M0004', 'Y', 'Y', 'Y', 'N', 'N');
+INSERT INTO Genres VALUES('M0006', 'Y', 'Y', 'Y', 'N', 'N');
+INSERT INTO Genres VALUES('M0007', 'Y', 'Y', 'N', 'N', 'N');
+INSERT INTO Genres VALUES('M0008', 'N', 'Y', 'N', 'N', 'N');
+INSERT INTO Genres VALUES('M0009', 'N', 'Y', 'N', 'N', 'N');
+INSERT INTO Genres VALUES('M0010', 'N', 'Y', 'N', 'N', 'N');
+INSERT INTO Genres VALUES('M0011', 'N', 'Y', 'N', 'N', 'Y');
+INSERT INTO Genres VALUES('M0012', 'N', 'Y', 'N', 'N', 'Y');
+INSERT INTO Genres VALUES('M0013', 'N', 'Y', 'N', 'N', 'Y');
+INSERT INTO Genres VALUES('M0015', 'N', 'Y', 'N', 'N', 'Y');
+INSERT INTO Genres VALUES('M0016', 'N', 'Y', 'N', 'N', 'Y');
+INSERT INTO Genres VALUES('M0017', 'N', 'Y', 'N', 'N', 'Y');
+INSERT INTO Genres VALUES('M0018', 'N', 'Y', 'N', 'N', 'Y');
+INSERT INTO Genres VALUES('M0019', 'Y', 'Y', 'N', 'N', 'Y');
+INSERT INTO Genres VALUES('M0020', 'Y', 'Y', 'N', 'N', 'Y');
+INSERT INTO Genres VALUES('M0021', 'Y', 'Y', 'N', 'N', 'Y');
+INSERT INTO Genres VALUES('M0022', 'Y', 'Y', 'N', 'N', 'Y');
 
 --TV Genres
 INSERT INTO TV_Genres VALUES(1, 'Y', 'N', 'Y', 'Y', 'Y');
@@ -80,6 +151,7 @@ INSERT INTO TV_Genres VALUES(3, 'N', 'N', 'N', 'Y', 'Y');
 INSERT INTO TV_Genres VALUES(4, 'N', 'Y', 'N', 'N', 'Y');
 INSERT INTO TV_Genres VALUES(5, 'N', 'N', 'N', 'N', 'Y');
 INSERT INTO TV_Genres VALUES(6, 'Y', 'Y', 'N', 'N', 'N');
+INSERT INTO TV_Genres VALUES(0, 'N', 'Y', 'Y', 'N', 'Y');
 
 --Industry Person
 INSERT INTO Industry_Person VALUES(1, 'Jennifer', 'Garner');
@@ -92,6 +164,17 @@ INSERT INTO Industry_Person VALUES(7, 'Stark', 'Drago');
 INSERT INTO Industry_Person VALUES(8, 'Max', 'Coal');
 INSERT INTO Industry_Person VALUES(9, 'Walter', 'White');
 INSERT INTO Industry_Person VALUES(10, 'Carlton', 'Drake');
+INSERT INTO Industry_Person VALUES(11, 'Jon ', 'Favreau');
+INSERT INTO Industry_Person VALUES(12, 'Shane ', 'Black');
+INSERT INTO Industry_Person VALUES(13, 'Louis ', 'Leterrier');
+INSERT INTO Industry_Person VALUES(14, 'Kenneth ', 'Branagh');
+INSERT INTO Industry_Person VALUES(15, 'Alan ', 'Taylor');
+INSERT INTO Industry_Person VALUES(16, 'Taika ', 'Waititi');
+INSERT INTO Industry_Person VALUES(17, 'Joe ', 'Johnston');
+INSERT INTO Industry_Person VALUES(18, 'Joss ', 'Whedon');
+INSERT INTO Industry_Person VALUES(19, 'Anthony', 'Russo');
+INSERT INTO Industry_Person VALUES(20, 'Joe ', 'Russo');
+INSERT INTO Industry_Person VALUES(21, 'James ', 'Gunn');
 
 --Directs
 INSERT INTO Directs VALUES(6, 'M0002');
@@ -99,6 +182,23 @@ INSERT INTO Directs VALUES(8, 'M0003');
 INSERT INTO Directs VALUES(7,  'M0005');
 INSERT INTO Directs VALUES(9, 'M0005');
 INSERT INTO Directs VALUES(10, 'T0002');
+INSERT INTO Directs VALUES(11, 'M0004');
+INSERT INTO Directs VALUES(11, 'M0006');
+INSERT INTO Directs VALUES(12, 'M0007');
+INSERT INTO Directs VALUES(13, 'M0008');
+INSERT INTO Directs VALUES(14, 'M0009');
+INSERT INTO Directs VALUES(15, 'M0010');
+INSERT INTO Directs VALUES(16, 'M0011');
+INSERT INTO Directs VALUES(16, 'M0012');
+INSERT INTO Directs VALUES(17, 'M0013');
+INSERT INTO Directs VALUES(18, 'M0015');
+INSERT INTO Directs VALUES(19, 'M0016');
+INSERT INTO Directs VALUES(19, 'M0017');
+INSERT INTO Directs VALUES(19, 'M0018');
+INSERT INTO Directs VALUES(19, 'M0019');
+INSERT INTO Directs VALUES(21, 'M0020');
+INSERT INTO Directs VALUES(21, 'M0021');
+INSERT INTO Directs VALUES(21, 'M0022');
 
 --Awards
 INSERT INTO Awards VALUES(1, 'Best Picture', 'Academy Awards', 2010, 'M0001');
@@ -142,6 +242,24 @@ INSERT INTO Media_Preferences VALUES(3,'T0002', 'N');
 INSERT INTO Media_Preferences VALUES(4,'M0001', 'Y');
 INSERT INTO Media_Preferences VALUES(2,'M0002', 'Y');
 INSERT INTO Media_Preferences VALUES(5,'T0001', 'Y');
+INSERT INTO Media_Preferences VALUES(1,'T0017', 'Y');
+INSERT INTO Media_Preferences VALUES(1,'T0018', 'Y');
+INSERT INTO Media_Preferences VALUES(1,'T0019', 'Y');
+INSERT INTO Media_Preferences VALUES(1,'T0020', 'Y');
+INSERT INTO Media_Preferences VALUES(2,'T0021', 'N');
+INSERT INTO Media_Preferences VALUES(2,'T0022', 'N');
+INSERT INTO Media_Preferences VALUES(2,'M0004', 'N');
+INSERT INTO Media_Preferences VALUES(3,'M0006', 'N');
+INSERT INTO Media_Preferences VALUES(3,'M0007', 'Y');
+INSERT INTO Media_Preferences VALUES(3,'M0008', 'Y');
+INSERT INTO Media_Preferences VALUES(3,'M0009', 'Y');
+INSERT INTO Media_Preferences VALUES(3,'M0010', 'Y');
+INSERT INTO Media_Preferences VALUES(4,'M0011', 'Y');
+INSERT INTO Media_Preferences VALUES(4,'T0012', 'N');
+INSERT INTO Media_Preferences VALUES(4,'T0011', 'N');
+INSERT INTO Media_Preferences VALUES(5,'M0015', 'N');
+INSERT INTO Media_Preferences VALUES(5,'M0017', 'N');
+INSERT INTO Media_Preferences VALUES(5,'M0004', 'N');
 
 --TV Preferences
 INSERT INTO TV_Preferences VALUES(1,1, 'Y');
@@ -154,6 +272,21 @@ INSERT INTO TV_Preferences VALUES(2,4, 'Y');
 INSERT INTO TV_Preferences VALUES(5,3, 'Y');
 
 --Views
+INSERT INTO Views VALUES(11, 1,'M0011', to_date('01-02-2020', 'mm-dd-yyyy'),'Florida',4569 );
+INSERT INTO Views VALUES(12, 1,'T0022', to_date('02-09-2021', 'mm-dd-yyyy'),'Florida',3465 );
+INSERT INTO Views VALUES(13, 2,'T0018', to_date('09-08-2020', 'mm-dd-yyyy'),'Illinois',3489 );
+INSERT INTO Views VALUES(14, 2,'M0011', to_date('01-23-2020', 'mm-dd-yyyy'),'Illinois',4098 );
+INSERT INTO Views VALUES(15, 2,'T0021', to_date('12-01-2019', 'mm-dd-yyyy'),'Illinois',3245 );
+INSERT INTO Views VALUES(16, 3,'M0018', to_date('09-10-2020', 'mm-dd-yyyy'),'Delhi',4512 );
+INSERT INTO Views VALUES(17, 3,'M0021', to_date('08-07-2020', 'mm-dd-yyyy'),'Delhi',4590 );
+INSERT INTO Views VALUES(18, 3,'T0012', to_date('03-21-2021', 'mm-dd-yyyy'),'Delhi',3789 );
+INSERT INTO Views VALUES(19, 4,'M0004', to_date('01-13-2021', 'mm-dd-yyyy'),'California',5367 );
+INSERT INTO Views VALUES(20, 5,'M0006', to_date('04-12-2021', 'mm-dd-yyyy'),'California',4096 );
+INSERT INTO Views VALUES(21, 2,'M0016', to_date('03-21-2021', 'mm-dd-yyyy'),'Illinois',4865 );
+INSERT INTO Views VALUES(22, 2,'M0017', to_date('01-23-2020', 'mm-dd-yyyy'),'Illinois',3278 );
+INSERT INTO Views VALUES(23, 2,'M0018', to_date('09-08-2020', 'mm-dd-yyyy'),'Illinois',3121 );
+INSERT INTO Views VALUES(24, 1,'M0001', to_date('03-21-2021', 'mm-dd-yyyy'),'Florida',4590 );
+INSERT INTO Views VALUES(25, 2,'M0001', to_date('03-21-2021', 'mm-dd-yyyy'),'Illinois',3121 );
 
 
 --Acts In
