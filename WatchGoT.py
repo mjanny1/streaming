854e450d17f8dc15021bd2fc968adb7405371694
Matthew Janny <mattjanny1@gmail.com>
2021-05-06 00:05:12 -0400

    Mimic watching GOT from Python

--- WatchGoT.py
+++ WatchGoT.py
@@ -1,5 +1,10 @@
 import cx_Oracle
 import time 
+import random
+from datetime import date
+
+profile_id = 4 #This is Rodger according to the Fake DB Data
+
 
 dsn = cx_Oracle.makedsn(
     'csdb2.csc.villanova.edu', 
@@ -29,16 +34,40 @@ if episode > 10:
     exit (1)
 
 print ("You are now watching!")
-input("Press Ctrl-C to stop watching...")
+todays_date = date.today()
 time_watched = 0
+response = 1
+print ("Press Ctrl-C to Stop Watching: ")
 while True:
     try:
+        time_watched += 1
         time.sleep(1)
-        time_watched = time_watched + 1
     except KeyboardInterrupt:
         break
-
 print ("You watched for " + str(time_watched) + " seconds")
+
+
+## Create Insert String
+# Create View ID
+view_id = random.randint(0, 100000000)
+
+# Get Media ID
+get_media_id="select media_id from tv_episodes where season_number = " + str(season) + " and season_episode_number = " + str(episode) + " and tv_show_id = 0"
+c.execute(get_media_id)
+for row in c: media_id_string = row
+media_id_string = str(media_id_string)
+media_id_string = media_id_string.replace("('", "")
+media_id = media_id_string.replace("',)", "")
+
+# Profile ID to String
+profile_id = str(profile_id)
+
+# Create Insert String
+insert="INSERT INTO Views VALUES(" + str(view_id) + ", " + profile_id + ",'" + media_id + "', to_date('" + str(todays_date) + "', 'yyyy-mm-dd'),'Philadelphia', " + str(time_watched) + ")"
+c.execute(insert)
+result = conn.commit()
+if result == 0:
+    print ("View successfully stored in Database")
 
 print ("Thanks for Watching!")
 conn.close()
